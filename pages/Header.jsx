ce74e0f42f4debd61336faff05a473b6caecd352
lomowang <frees1102@gmail.com>
2025-02-19 21:28:27 +0800

    20250219

--- pages/Header.jsx
+++ pages/Header.jsx
@@ -1,9 +1,6 @@
-// 首頁
-
 "use client";
 import React, { useState, useEffect } from "react";
 import { motion } from "framer-motion";
-import { fadeIn } from "@/variants";
 import MobileNav from "./MobileNav";
 import Nav from "./Nav";
 import Link from "next/link";
@@ -13,27 +10,34 @@ const Header = () => {
 
   useEffect(() => {
     const handleScroll = () => {
-      setScrolled(window.scrollY > 0);
+      if (window.scrollY > 0 && !scrolled) {
+        setScrolled(true);
+      } else if (window.scrollY === 0 && scrolled) {
+        setScrolled(false);
+      }
     };
 
     window.addEventListener("scroll", handleScroll);
-
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [scrolled]);
 
   return (
     <header
-      className={` top-0 z-50 max-w-[1980px] py-2 px-4 m-2 xl:py-10 text-sm text-center items-center md:justify-between lg:px-8 `}
+      className={`top-0 z-50 max-w-[1980px] py-2 px-4 m-2 xl:py-10 text-sm text-center md:justify-between lg:px-8 ${
+        scrolled
+          ? "bg-white shadow-md transition-all duration-300"
+          : "bg-transparent"
+      }`}
     >
-      <h1 className="text-center  font-bold text-[24px]">
+      <h1 className="text-center font-bold text-[24px]">
         <Link href="/">Posak Jodian</Link>
       </h1>
+      {/* 桌面版導覽列 */}
       <Nav
-        containerStyles="xl:flex items-center hidden justify-center "
-        listStyles="flex gap-2 text-sm top-20 leading-8 text-gray-400 "
+        containerStyles="hidden xl:flex items-center justify-center"
+        listStyles="flex gap-2 text-sm leading-8 text-gray-400"
       />
+      {/* 行動版導覽列 */}
       <div className="xl:hidden">
         <MobileNav />
       </div>
--- pages/Hero.jsx
+++ pages/Hero.jsx
@@ -1,10 +1,10 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import Projectswiper from "./Projectswiper";
 
 const Hero = () => {
   return (
-    <section className="bg-black w-full h-screen flex justify-center items-center">
-      <div className="w-full h-full">
+    <section className="bg-black w-full min-h-screen flex justify-center items-center">
+      <div className="w-full max-w-7xl h-[80vh]">
         <Projectswiper />
       </div>
     </section>
