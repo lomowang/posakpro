5adc7fedf848393737ef41cb673bbc7ad553db4b
lomowang <frees1102@gmail.com>
2025-03-19 00:20:56 +0800

    20250319

--- pages/Hero.jsx
+++ pages/Hero.jsx
@@ -3,8 +3,8 @@ import Projectswiper from "./Projectswiper";
 
 const Hero = () => {
   return (
-    <section className="bg-black w-full min-h-[30vh] md:min-h-[60vh] lg:min-h-[0vh] flex justify-center items-center">
-      <div className="w-full max-w-7xl h-full ">
+    <section className="bg-black w-full min-h-[30vh] md:min-h-[60vh] lg:min-h-[80vh] flex justify-center items-center">
+      <div className="w-full sm:max-w-3xl lg:max-w-5xl h-full">
         <Projectswiper />
       </div>
     </section>
--- pages/Projectswiper.jsx
+++ pages/Projectswiper.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay, EffectFade, Navigation } from "swiper/modules";
 import "swiper/css";
@@ -13,25 +13,55 @@ const work = [
     url: "/workpage/Simpleriver",
   },
   {
+    Image: "/pro/Simpleriver/river_3.png",
+    name: "river,2020",
+    url: "/workpage/Simpleriver",
+  },
+  {
+    Image: "/pro/Simpleriver/river_6.png",
+    name: "river,2020",
+    url: "/workpage/Simpleriver",
+  },
+  {
     Image: "/pro/Misafafahiyan_14.jpeg",
     name: "Misafafahiyan,2020",
     url: "/workpage/Misafafahiyan",
   },
   {
-    Image: "/pro/lacke.png",
-    name: "lacke,2018",
+    Image: "/pro/Misafafahiyan/Misafafahiyan_1.jpeg",
+    name: "Misafafahiyan,2020",
+    url: "/workpage/Misafafahiyan",
+  },
+  {
+    Image: "/pro/Misafafahiyan/Misafafahiyan_9.jpeg",
+    name: "Misafafahiyan,2020",
+    url: "/workpage/Misafafahiyan",
+  },
+  {
+    Image: "/pro/Lakec/lakec_5.png",
+    name: "lake,2018",
+    url: "/workpage/Lakec",
+  },
+  {
+    Image: "/pro/Lakec/lakec_2.jpg",
+    name: "lake,2018",
+    url: "/workpage/Lakec",
+  },
+  {
+    Image: "/pro/Lakec/lakec_3.png",
+    name: "lake,2018",
     url: "/workpage/Lakec",
   },
 ];
 
 const Projectswiper = () => {
-  return (
-    <div className="w-full h-full relative overflow-hidden">
-      {/* 導航按鈕 */}
-      <div className="swiper-button-prev"></div>
-      <div className="swiper-button-next"></div>
+  const swiperRef = useRef(null); // Swiper 參考
 
+  return (
+    <div className="relative w-full h-full overflow-hidden">
+      {/* Swiper */}
       <Swiper
+        ref={swiperRef}
         slidesPerView={1}
         spaceBetween={0}
         autoplay={{
@@ -43,29 +73,21 @@ const Projectswiper = () => {
         fadeEffect={{
           crossFade: true,
         }}
-        navigation={{
-          nextEl: ".swiper-button-next",
-          prevEl: ".swiper-button-prev",
-        }}
-        breakpoints={{
-          640: { slidesPerView: 1 },
-          1024: { slidesPerView: 1 },
-          1280: { slidesPerView: 1 },
-        }}
+        navigation={true} // 啟用內建導航按鈕
         modules={[Autoplay, EffectFade, Navigation]}
         className="heroSwiper"
       >
         {work.map((slide, index) => (
           <SwiperSlide key={index}>
             <Link href={slide.url}>
-              <div className="relative w-full h-full cursor-pointer overflow-hidden">
+              <div className="relative w-full h-[30vh] md:h-[60vh] lg:h-[80vh] overflow-hidden">
                 <img
                   src={slide.Image}
                   alt={slide.name}
-                  className="w-full h-full object-cover"
+                  className="w-full h-full object-contain"
                 />
                 {/* 主題名稱 */}
-                <div className="absolute bottom-4 left-4 bg-black bg-opacity-50 text-white px-4 py-2 rounded">
+                <div className="absolute bottom-4 left-4 bg-black bg-opacity-70 text-white px-4 py-2 rounded-lg shadow-lg">
                   <h3 className="text-[16px] md:text-[18px] lg:text-[18px] tracking-wide font-semibold">
                     {slide.name}
                   </h3>
