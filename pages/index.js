e8fafb2db6abe9490ca7e7ec39f5ad4832d0e837
lomowang <frees1102@gmail.com>
2025-03-19 20:43:15 +0800

    20250319

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import Projectswiper from "./Projectswiper";
 
 export default function Home() {
   return (
-    <main>
+    <main className="min-h-[30vh] md:min-h-[60vh] lg:min-h-[80vh]">
       <Hero />
     </main>
   );
