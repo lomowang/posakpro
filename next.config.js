92db506ffbb8c92f63cfa46cbd69e108fdf421d3
lomowang <frees1102@gmail.com>
2026-07-13 22:17:20 +0800

    Add shareable project tracker

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: "/project-tracker",
+        destination: "/project-tracker/index.html",
+        permanent: false,
+      },
+    ];
+  },
   images: {
     formats: ["image/avif", "image/webp"],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2560],
